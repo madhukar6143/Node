78a3a47d9a8c8148edb55424dc96f15a323816d6
Madhukar <madhukar@sg-lt-233.senecaglobal.net>
2023-02-13 11:41:20 +0530

    joi validations added

--- module/joi.js
+++ module/joi.js
@@ -1,7 +1,13 @@
 const Joi = require('joi');
 
 const usernameSchema = Joi.string().alphanum().min(3).max(30).required();
-const passwordSchema = Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required();
+const passwordSchema = Joi.string()
+                           .min(8)
+                           .max(30)
+                           .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=.{8,})'))
+                           .required()
+                           .error(new Error('Password must contain at least 8 characters, including uppercase and lowercase letters, numbers, and special characters'));
+                           ;
 
 exports.validateUsername = (username) => {
     const { error, value } = usernameSchema.validate(username);
